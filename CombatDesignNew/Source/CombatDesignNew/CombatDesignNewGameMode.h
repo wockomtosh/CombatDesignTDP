217cfef479886ff66d4f0e17046d62cb65631064
Bill Zhou (Lab) <yuxiao-zhou@outlook.com>
2025-01-17 16:13:57 -0700

    Rebase project to "CombatDesignNew"

---
+++ CombatDesignNew/Source/CombatDesignNew/CombatDesignNewGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatDesignNewGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACombatDesignNewGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACombatDesignNewGameMode();
+};
+
+
+
