d9189f7665b2765ab560432f277ee07f958e252b
Bill Zhou (Lab) <yuxiao-zhou@outlook.com>
2025-04-22 16:25:36 -0600

    ADD minor enemies to the map

--- CombatDesignNew/Source/CombatDesignNew/CombatDesignNewGameMode.cpp
+++ CombatDesignNew/Source/CombatDesignNew/CombatDesignNewGameMode.cpp
@@ -7,7 +7,7 @@
 ACombatDesignNewGameMode::ACombatDesignNewGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/BP_PlayerCharacter.BP_PlayerCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
